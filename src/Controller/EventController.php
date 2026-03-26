e6a951ac432d624bf3a09b7459da9cef7333e36e
yosrabhiri <yosrabhiri16@gmail.com>
2026-03-26 20:30:29 +0100

    filtre evets

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\EventRepository;
 
+
 final class EventController extends AbstractController
 {
     #[Route('/event', name: 'app_event')]
-    public function index(EventRepository $eventRepository): Response
+    public function index(Request $request, EventRepository $eventRepository): Response
     {
-        $events = $eventRepository->findAll();
+        $location = $request->query->get('location');
+        $date = $request->query->get('date');
+        $seats = $request->query->get('seats');
+
+        $events = $eventRepository->searchEvents($location, $date, $seats);
 
         return $this->render('event/index.html.twig', [
             'events' => $events,
--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -15,6 +15,33 @@ class EventRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Event::class);
     }
+    public function searchEvents(?string $location, ?string $date, ?string $seats): array
+{
+    $qb = $this->createQueryBuilder('e');
+
+    if (!empty($location)) {
+        $qb->andWhere('e.location LIKE :location')
+           ->setParameter('location', '%' . $location . '%');
+    }
+
+    if (!empty($date)) {
+        $start = new \DateTime($date . ' 00:00:00');
+        $end = new \DateTime($date . ' 23:59:59');
+
+        $qb->andWhere('e.date BETWEEN :start AND :end')
+           ->setParameter('start', $start)
+           ->setParameter('end', $end);
+    }
+
+    if (!empty($seats)) {
+        $qb->andWhere('e.seats >= :seats')
+           ->setParameter('seats', (int) $seats);
+    }
+
+    return $qb->orderBy('e.date', 'ASC')
+              ->getQuery()
+              ->getResult();
+}
 
 //    /**
 //     * @return Event[] Returns an array of Event objects
